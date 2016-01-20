60b961b611a25cc4563e63d4246d2db31d7d821c
bl2012 <blulightning2012@hotmail.com>
2016-01-20 08:41:24 -0500

    Beginning Movement
    Mario can now walk and run

---
+++ Mario 1-1/Assets/Scripts/PixelPerfectCamera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PixelPerfectCamera : MonoBehaviour {
+
+	public static float pixelsToUnits = 1f;
+	public static float scale = 1f;
+
+	public Vector2 nativeRes = new Vector2(256, 240);
+
+	void Awake () {
+		var camera = GetComponent<Camera> ();
+
+		if (camera.orthographic) {
+			scale = Screen.height / nativeRes.y;
+			pixelsToUnits *= scale;
+			camera.orthographicSize = (Screen.height / 2.0f) / pixelsToUnits;
+		}
+	}
+
+}
---
+++ Mario 1-1/Assets/Scripts/script_tiling.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class script_tiling : MonoBehaviour {
+
+	public int textureSize = 16;
+	public int groundHeight = 2; // number of blocks tall
+	public float groundWidth;
+	public float newWidth;
+
+	// Use this for initialization
+	void Start () {
+
+		groundWidth = gameObject.transform.localScale.x;
+		newWidth = groundWidth - (groundWidth % textureSize);
+
+		transform.localScale = new Vector3 (newWidth, groundHeight * textureSize, 1);
+
+		GetComponent<Renderer> ().material.mainTextureScale = new Vector3 (newWidth / textureSize, groundHeight, 1);
+
+	}
+
+}
